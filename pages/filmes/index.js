680d983d95df86263ae10e1c482c4828719c4e3c
pedroblima <pedrob@iesb.edu.br>
2023-04-13 20:17:42 -0300

    ApiFilmes

--- pages/filmes/index.js
+++ pages/filmes/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Pagina from '../../components/Pagina'
+import ApiFilmes from '../../services/ApiFilmes'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const index = () => {
-  return (
-    <div>index</div>
+
+const hooks = () => {
+
+
+    const [Filmes, setFilmes] = useState([])
+
+    useEffect(()=>{
+
+        ApiFilmes.get('/movie').then(resultado=>{
+           setFilmes(resultado.data.dados)
+        })
+    }, [])
+    
+    return (
+    
+    
+    <Pagina titulo='Filmes'>
+        <h1>Hooks</h1>
+        {Filmes.map(item => (
+          <p>{item.nome}</p>
+        ))}
+    
+    
+    </Pagina>
   )
 }
 
-export default index
+export default hooks
---
+++ services/ApiFilmes.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const ApiFilmes = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  Headers: {
+    Authorization: 'Bearer  eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmZhYjllOTU2MjQ4NmRkODZiNTc2MDljZmRlNTJmYyIsInN1YiI6IjY0Mzg0Y2I2NGE1MmY4MDBmNGY3NWY1ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hQ84pzlF1-m3TRwWIfR_ezxJDpXIs-QJTEKBSSL3_cY'
+  }
+})
+  
+export default ApiFilmes
